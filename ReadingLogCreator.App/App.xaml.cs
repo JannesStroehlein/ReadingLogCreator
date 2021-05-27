be5a16896519544e891b3fbd9c1c942b7baad2a1
Jannes Ströhlein <jati.stroehlein@gmail.com>
2021-05-27 14:45:29 +0200

    Package setup, boilerplate code

--- ReadingLogCreator.App/App.xaml.cs
+++ ReadingLogCreator.App/App.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using HandyControl.Data;
+using HandyControl.Tools;
+using System;
 using System.Windows;
 
 namespace ReadingLogCreator.App
@@ -13,5 +10,22 @@ namespace ReadingLogCreator.App
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            ConfigHelper.Instance.SetLang("en");
+            this.UpdateSkin(SkinType.Dark);
+        }
+        internal void UpdateSkin(SkinType skin)
+        {
+            this.Resources.MergedDictionaries.Clear();
+            this.Resources.MergedDictionaries.Add(new ResourceDictionary
+            {
+                Source = new Uri($"pack://application:,,,/HandyControl;component/Themes/Skin{skin.ToString()}.xaml")
+            });
+            this.Resources.MergedDictionaries.Add(new ResourceDictionary
+            {
+                Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
+            });
+        }
     }
 }
